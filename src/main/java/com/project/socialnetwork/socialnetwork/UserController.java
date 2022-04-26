605310f6dadb7f586ac963f1ffc9c4718305113b
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-04-26 04:53:44 -0300

    aplicando retorno dos valores  endpoint de listar todos os usuarios

--- src/main/java/com/project/socialnetwork/socialnetwork/UserController.java
+++ src/main/java/com/project/socialnetwork/socialnetwork/UserController.java
@@ -1,16 +1,27 @@
 package com.project.socialnetwork.socialnetwork;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import com.project.socialnetwork.dto.CreateUserRequest;
 
 @Path("/users")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class UserController {
    
 	@POST
 	public Response createUser(CreateUserRequest userRequest) {
+		return Response.ok(userRequest).build();
+	}
+	
+	@GET
+	public Response ListAllUsers() {
 		return Response.ok().build();
 	}
 }
