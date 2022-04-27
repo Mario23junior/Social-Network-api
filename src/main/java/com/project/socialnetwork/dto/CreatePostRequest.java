d2764e1c49a7e23fc4e77bc7607ee2d275d89b11
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-04-27 04:51:31 -0300

    Criando tabela de trafego de post

---
+++ src/main/java/com/project/socialnetwork/dto/CreatePostRequest.java
@@ -0,0 +1,19 @@
+package com.project.socialnetwork.dto;
+
+public class CreatePostRequest {
+
+	private String text;
+
+	public CreatePostRequest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+}
