5187ec05b876bec7d93665656fd86f028dea2dbc
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-04-27 04:49:41 -0300

    Criando tabela no banco POSTS

---
+++ src/main/java/com/project/socialnetwork/model/posts.java
@@ -0,0 +1,68 @@
+package com.project.socialnetwork.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "POSTS")
+public class posts {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "post_text")
+	private String text;
+	
+	@Column(name = "datetime")
+	private LocalDateTime datetime;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	public posts() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public LocalDateTime getDatetime() {
+		return datetime;
+	}
+
+	public void setDatetime(LocalDateTime datetime) {
+		this.datetime = datetime;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+}
