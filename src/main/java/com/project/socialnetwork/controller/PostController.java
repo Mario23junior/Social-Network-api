351464b2e9d92c6ac6c7e77c0af3cc66c6025c48
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-04-27 05:10:23 -0300

    Salvando text no banco de dados

--- src/main/java/com/project/socialnetwork/controller/PostController.java
+++ src/main/java/com/project/socialnetwork/controller/PostController.java
@@ -1,6 +1,7 @@
 package com.project.socialnetwork.controller;
 
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -10,7 +11,10 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import com.project.socialnetwork.dto.CreatePostRequest;
+import com.project.socialnetwork.model.Post;
 import com.project.socialnetwork.model.User;
+import com.project.socialnetwork.repository.PostRepository;
 import com.project.socialnetwork.repository.UserRepository;
 
 @Path("/users/{userId}/posts")
@@ -19,21 +23,49 @@ import com.project.socialnetwork.repository.UserRepository;
 public class PostController {
   
 	private UserRepository repository;
+	private PostRepository postRepository;
 	
 	@Inject
-	public PostController(UserRepository repository) {
-		this.repository = repository;
- 	}
+	public PostController(UserRepository repository, PostRepository postRepository) {
+ 		this.repository = repository;
+		this.postRepository = postRepository;
+	}
 	
 	@POST
-	public Response savePost(@PathParam("userId") Long userId) {
+	@Transactional
+	public Response savePost(@PathParam("userId") Long userId, CreatePostRequest request) {
 		User user = repository.findById(userId);
 		if(user == null) {
 			return Response.status(Response.Status.NOT_FOUND).build();
 		}
+		
+		Post post = new Post();
+		post.setText(request.getText());
+ 		post.setUser(user);
+ 
+		
+ 		postRepository.persist(post);
+ 		
 		return Response.status(Response.Status.CREATED).build();
 	}
 	
+
+	public UserRepository getRepository() {
+		return repository;
+	}
+
+	public void setRepository(UserRepository repository) {
+		this.repository = repository;
+	}
+
+	public PostRepository getPostRepository() {
+		return postRepository;
+	}
+
+	public void setPostRepository(PostRepository postRepository) {
+		this.postRepository = postRepository;
+	}
+
 	@GET
 	public Response listPost(@PathParam("userId") Long userId) {
 		User user = repository.findById(userId);
