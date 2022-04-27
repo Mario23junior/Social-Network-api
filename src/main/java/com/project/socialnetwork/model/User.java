4315a57c7f0045e1b96289b73a33436393f8bfc0
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-04-27 00:07:41 -0300

    Criando modelo do banco de dodos de Users

--- src/main/java/com/project/socialnetwork/model/User.java
+++ src/main/java/com/project/socialnetwork/model/User.java
@@ -6,34 +6,40 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
 @Entity
 @Table(name = "users")
-public class User {
-   
+public class User extends PanacheEntityBase {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;	
-	private String nome;
+	private Long id;
+	private String name;
 	private Integer age;
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
-	public String getNome() {
-		return nome;
+
+	public String getName() {
+		return name;
 	}
-	public void setNome(String nome) {
-		this.nome = nome;
+
+	public void setName(String name) {
+		this.name = name;
 	}
+
 	public Integer getAge() {
 		return age;
 	}
+
 	public void setAge(Integer age) {
 		this.age = age;
 	}
-	
-	
+
 }
