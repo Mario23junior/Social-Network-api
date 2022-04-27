759b07736d96f4c49df2062b2de1ac9560e42515
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-04-27 01:13:23 -0300

    Atualizando e deletando usuarios

--- src/main/java/com/project/socialnetwork/socialnetwork/UserController.java
+++ src/main/java/com/project/socialnetwork/socialnetwork/UserController.java
@@ -2,12 +2,16 @@ package com.project.socialnetwork.socialnetwork;
 
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import com.project.socialnetwork.dto.CreateUserRequest;
 import com.project.socialnetwork.model.User;
@@ -36,4 +40,35 @@ public class UserController {
 		PanacheQuery<PanacheEntityBase> query = User.findAll();
 		return Response.ok(query.list()).build();
 	}
+	
+	@DELETE
+	@Transactional
+	@Path("{id}")
+	public Response deleteUser(@PathParam("id") Long id) {
+	  User user = User.findById(id);
+	  
+	  if(user != null) {
+		  user.delete();
+	  } else {
+		  return Response.status(Status.NOT_FOUND).build();
+	  }
+	  return Response.ok().build();
+	}
+	
+	@PUT
+	@Transactional
+	@Path("{id}")
+	public Response update(@PathParam("id")Long id, CreateUserRequest userData) {
+		User user = User.findById(id);
+		
+		if(user != null) {
+			user.setName(userData.getName());
+			user.setAge(userData.getAge());
+			return Response.ok().build();
+ 		}
+		return Response.status(Response.Status.NOT_FOUND).build();
+	}
 }
+
+ 
+
