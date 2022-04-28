cc98c25986afd0ec7d2b27999da2b31c68636667
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-04-28 06:12:38 -0300

    Implementando metódo para seguir um usuario

--- src/main/java/com/project/socialnetwork/controller/FollowerController.java
+++ src/main/java/com/project/socialnetwork/controller/FollowerController.java
@@ -2,10 +2,17 @@ package com.project.socialnetwork.controller;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
+import com.project.socialnetwork.dto.FollowerRequest;
+import com.project.socialnetwork.model.Follower;
+import com.project.socialnetwork.model.User;
 import com.project.socialnetwork.repository.FollowerRepository;
 import com.project.socialnetwork.repository.UserRepository;
 
@@ -23,6 +30,20 @@ public class FollowerController {
 		this.userRepository = userRepository;
 	}
 	
-	
+	@PUT
+	public Response followerUser(@PathParam("userId") Long userId ,FollowerRequest request) {
+		User user = userRepository.findById(userId);
+		if(user == null) {
+			return Response.status(Status.NOT_FOUND).build();
+		} 
+		
+		User follower = userRepository.findById(request.getFollowerId());
+		
+		Follower entity = new Follower();
+		entity.setUser(user);
+		entity.setFollower(follower);
+		
+		return Response.status(Response.Status.NO_CONTENT).build();
+	}
 
 }
---
+++ src/main/java/com/project/socialnetwork/dto/FollowerRequest.java
@@ -0,0 +1,19 @@
+package com.project.socialnetwork.dto;
+
+public class FollowerRequest {
+    
+	private Long followerId;
+	
+	public FollowerRequest() {
+ 	}
+
+	public Long getFollowerId() {
+		return followerId;
+	}
+
+	public void setFollowerId(Long followerId) {
+		this.followerId = followerId;
+	}
+	
+	
+}
