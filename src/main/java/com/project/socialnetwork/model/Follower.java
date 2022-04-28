df1e971e5aefd5a6581bdf6384e3f77f98c17ccc
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-04-28 05:40:18 -0300

    Criando entidade de followers

---
+++ src/main/java/com/project/socialnetwork/model/Follower.java
@@ -0,0 +1,55 @@
+package com.project.socialnetwork.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "FOLLOWERS")
+public class Follower {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+    
+	@ManyToOne
+	@JoinColumn(name = "follower_id")
+	private User follower;
+
+	public Follower() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public User getFollower() {
+		return follower;
+	}
+
+	public void setFollower(User follower) {
+		this.follower = follower;
+	}
+
+}
